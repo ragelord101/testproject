99818a339cbbd7e81ce078d92606173e512f331d
ragelord101 <JoshiMedina101@gmail.com>
2025-05-22 17:00:33 -0400

    adding encrypt and decrypt feature branch

--- nodeproject.js
+++ nodeproject.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const path = require('path');
+
 
 
 app.set('view engine', 'ejs');
@@ -8,6 +10,38 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+var newalpha = '';
+
+function alphabetShift(key) {
+    key = parseInt(key);
+    for (let i = 0; i < alphabet.length; i++) {
+        let offset = (i + key) % alphabet.length;
+        newalpha += alphabet[offset];
+    }
+}
+
+function encode(message) {
+    let result = '';
+    message = message.toLowerCase();
+    for (let i = 0; i < message.length; i++) {
+        let index = alphabet.indexOf(message[i]);
+        result += newalpha[index];
+    }
+
+    return result;
+}
+
+function decode(message) {
+    let result = '';
+    message = message.toLowerCase();
+    for (let i = 0; i < message.length; i++) {
+        let index = newalpha.indexOf(message[i]);
+        result += alphabet[index];
+    }
+    
+    return result;
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World! welcome to our first publicly uploaded server')
@@ -17,7 +51,35 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
+app.get('/encryptform', (req, res) => {
+    res.render('encryptform')
+})
+
+app.post('/encrypt', (req, res) => {
+    const { inputtext, key } = req.body;
+    newalpha = '';
+    alphabetShift(key);
+    const encryptedText = encode(inputtext);
+    res.render('encryptform', { encryptedText, inputtext, key });
+
+})
+
+app.get('/decryptform', (req, res) => {
+    res.render('decryptform')
+})
+
+app.post('/decrypt', (req, res) => {
+    const { inputtext, key } = req.body;
+    newalpha = '';
+    alphabetShift(key);
+    const decryptedText = decode(inputtext);
+    console.log(decryptedText);
+    res.render('decryptform', { decryptedText, inputtext, key });
+
+})
+
 app.listen(
     port,
     () => console.log(`Its alive on http://localhost:${port}/`)
 )
+
